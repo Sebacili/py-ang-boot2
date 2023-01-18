2caf22a9a73d361a38b3826f81c3ab6818688123
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-01-18 18:09:41 +0000

    per il register

--- server_angular/project/src/app/register/register.component.ts
+++ server_angular/project/src/app/register/register.component.ts
@@ -9,3 +9,26 @@ export class RegisterComponent {
 
 }
 
+// aggiungendo questi linee di codice ha buggato il routerlink o la pag di registrazione in generale
+
+// import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.css']
+// })
+// export class RegisterComponent {
+//   form = {
+//     name: '',
+//     email: '',
+//     passw: '',
+//     repeatedpassw: ''
+//   }
+
+//   constructor( private http: HttpClient){}
+//   senddata(){
+//     this.http.post('https://3245-lukebasco121-pyangboot2-qrwb9ohee2q.ws-eu83.gitpod.io/info/post/registration', this.form).subscribe()
+//   }
+
+// }
