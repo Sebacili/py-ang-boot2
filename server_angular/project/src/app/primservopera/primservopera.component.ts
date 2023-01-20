af280e7e6cf825f3a3095e9538f1a142dcd624e7
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-01-20 22:29:38 +0000

    finally able to get info

--- server_angular/project/src/app/primservopera/primservopera.component.ts
+++ server_angular/project/src/app/primservopera/primservopera.component.ts
@@ -2,7 +2,8 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Data, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
+import { dataprimservopera } from 'src/models/dataprimservopera.model';
 
 // importare questo
 import { PrimservoperaService } from 'src/services/primservopera.service';
@@ -14,8 +15,27 @@ import { PrimservoperaService } from 'src/services/primservopera.service';
 })
 export class PrimservoperaComponent implements OnInit{
 
-  form!: FormGroup;
-  errorMessage!: string;
+
+  dati: dataprimservopera[] = [];
+  tecnica: string ='';
+  // save value
+  onClick(t: HTMLInputElement ) 
+  {
+    this.tecnica = (t.value);
+    this.sendData(this.tecnica);
+    return false;
+  }
+
+  sendData(tecnica: string) { let body: HttpParams = new HttpParams().appendAll({tecnica : tecnica})
+    this.http.post<dataprimservopera[]>('https://3245-lukebasco121-pyangboot2-iflf8mih949.ws-eu83.gitpod.io/api/tecnica_museo','',{
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }),
+      params: body,
+      responseType: "json"
+    }).subscribe(data => this.dati = data);
+  }
+  
 //////////////
   //PrimservoperaService: PrimservoperaService
   constructor(private PrimservoperaService: PrimservoperaService, private http: HttpClient, private fb: FormBuilder, private router: Router) {
@@ -23,43 +43,15 @@ export class PrimservoperaComponent implements OnInit{
    // per prendere i dati da py
   mydata: any;
   ngOnInit(): void {
-    this.PrimservoperaService.getdata().subscribe((data: any) => {
+    this.PrimservoperaService.getdata().pipe(take(1)).subscribe((data: any) => {
       this.mydata = data;
     });
     
-    this.form = this.fb.group({
-      tecnica: ["", [Validators.required]]
-    });
+    // this.form = this.fb.group({
+    //   tecnica: ["", [Validators.required]]
+    // });
   }
 //////////////
-
-
-  url : string = 'https://3245-lukebasco121-pyangboot2-iflf8mih949.ws-eu83.gitpod.io/api/tecnica_museo';
-  submit() {
-    let body: HttpParams = new HttpParams();
-    body = body.appendAll({
-      tecnica: this.form.value.tecnica
-    });
-
-    this.http.post<Data>(this.url, '', {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      params: body,
-      responseType: "json"
-    }).subscribe(data => {
-      console.log(data['data']);
-
-      if(data['url'] != null) {
-        this.router.navigate([data['url']]);
-      } else {
-        this.errorMessage = data['data'];
-      }
-    })
-  }
-
-
-
 
 
 }
