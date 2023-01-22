84febbd5e0b012a687985da65a4ebfdaeaa5a2ed
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-01-22 23:40:52 +0000

    dumb error

--- server_angular/project/src/app/intro-guide/intro-guide.component.ts
+++ server_angular/project/src/app/intro-guide/intro-guide.component.ts
@@ -15,12 +15,12 @@ export class IntroGuideComponent implements OnInit{
   ngOnInit(): void {
     this.isLocalStorageEmpty();
     this.addsuggFrom = this.fb.group({
-      nome: ["", [Validators.required]],
-      difficolta: ["", [Validators.required]],
-      n_gioc: ["", [Validators.required]],
-      scopo: ["", [Validators.required]],
+      name_user: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      message: ["", [Validators.required]],
     });
   }
+
 
   isLocalStorageEmpty(): boolean {
     if(localStorage.length === 0){
@@ -62,6 +62,17 @@ export class IntroGuideComponent implements OnInit{
     location.reload();
     alert("Your thoughts has been saved!");
 
+  }
+
+
+  email_ins: string='';
+  checkFormValue(key: string) {
+    const value = localStorage.getItem(email);
+    if (this.email_ins === value) {
+      console.log('The form value is equal to the value in local storage.');
+    } else {
+      console.log('The form value is not equal to the value in local storage.');
+    }
   }
 
 //   name_user: string ='';
